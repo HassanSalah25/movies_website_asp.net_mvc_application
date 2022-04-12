3a1c901d28f72fe28bd2b51fd0f452ee51cc7923
Hassan <salemhassan175@gmail.com>
2022-04-12 14:42:50 +0200

    Add project files.

---
+++ movies_website_asp.net_mvc_application/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace movies_website_asp.net_mvc_application.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ movies_website_asp.net_mvc_application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace movies_website_asp.net_mvc_application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
