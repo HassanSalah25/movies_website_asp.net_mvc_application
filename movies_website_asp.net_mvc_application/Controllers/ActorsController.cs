d08a585e79ab863fbc928820ae09a7c2d5a6ed50
Hassan <salemhassan175@gmail.com>
2022-04-13 04:09:52 +0200

    view update

---
+++ movies_website_asp.net_mvc_application/Controllers/ActorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using movies_website_asp.net_mvc_application.Data;
+
+namespace movies_website_asp.net_mvc_application.Controllers
+{
+    public class ActorsController : Controller
+    {
+        public readonly AppDBContext _context;
+        public ActorsController(AppDBContext context) => _context = context;
+        public IActionResult Index()
+        {
+            var AllActors = _context.Actors.ToList();
+            return View(AllActors);
+        }
+    }
+}
